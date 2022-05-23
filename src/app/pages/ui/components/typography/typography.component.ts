4f87222602e9e931786a2e918405d5899e43fa22
ZengGang315 <zoelasa1111@gmail.com>
2022-05-23 09:18:31 +0900

    ng2-admin

---
+++ src/app/pages/ui/components/typography/typography.component.ts
@@ -0,0 +1,13 @@
+import {Component, ViewEncapsulation} from '@angular/core';
+
+
+@Component({
+  selector: 'typography',
+  styles: [],
+  templateUrl: './typography.html',
+})
+export class Typography {
+
+  constructor() {
+  }
+}
